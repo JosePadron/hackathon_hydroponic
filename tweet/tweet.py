fd7a56a8468ccea6cc235b1ea6e822ca074166b7
Alejandro Moises <moisesaltamirano5@gmail.com>
2019-02-20 13:12:49 -0500

    Tweet Pics
    Code to upload pictures, need to update the path to pic

--- tweet/tweet.py
+++ tweet/tweet.py
@@ -8,4 +8,21 @@ A_token = "1097567943179804672-CUMJDtzUkHGL2SOniGMlpcfNZrn7uv"
 A_secret = "ZVdXNiNZVXALHsEN08kaOASXSFMOmy06NvhVtAkMEPwPs"
 
 myTweet = Twython(C_key,C_secret,A_token,A_secret)
-myTweet.update_status(status="Tweeting from my Fridge with Hydroponics")
+#myTweet.update_status(status="Tweeting from my Fridge with Hydroponics")
+
+from twython import Twython
+
+# fill in your 4 keys in following variables
+C_key = "IVoABm79IWmZ8S3L2gwUR7cgv"
+C_secret = "JxUTw6ixTtp4gl7kyfERaALqZ0MY9s9430dtAuM6DYyOu8Sbyu"
+A_token = "1097567943179804672-CUMJDtzUkHGL2SOniGMlpcfNZrn7uv"
+A_secret = "ZVdXNiNZVXALHsEN08kaOASXSFMOmy06NvhVtAkMEPwPs"
+
+myTweet = Twython(C_key,C_secret,A_token,A_secret)
+#myTweet.update_status(status="Tweeting from my Fridge")
+#twitter.update_status_with_media(status='Check out my Garden', media='/home/pi/Pictures/image.jpg')
+
+#insert image address below
+photo = open('/home/pi/Pictures/image.jpg', 'rb')
+response = myTweet.upload_media(media=photo)
+myTweet.update_status(status='Check out how my Hydroponics is looking! :)', media_ids=[response['media_id']])
