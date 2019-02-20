3a90351af64e61f7914ec22572bf4040d753e1a9
Jose R. Padron <Jose.Padron@ge.com>
2019-02-20 00:55:20 -0500

    remove firebase

--- hydroponicServer.js
+++ hydroponicServer.js
@@ -3,8 +3,6 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var os = require('os');
-var Firebase = require("firebase");
-var myDataRef = new Firebase('https://flickering-torch-9611.firebaseio.com/');
 var os = require('os');
 const Raspistill = require('node-raspistill').Raspistill;
 const camera = new Raspistill({
@@ -19,24 +17,6 @@ const camera = new Raspistill({
 var ON = 1
 var OFF = 0
 var lightState = OFF;
-
-//Set server ip
-myDataRef.child("server").set({
-  server_ip: ip_address('wlan0')
-});
-
-function ip_address(interface) {
-  var items = os.networkInterfaces()[interface] || [];
-
-  return items
-    .filter(function(item) {
-      return item.family.toLowerCase() == 'ipv4';
-    })
-    .map(function(item) {
-      return item.address;
-    })
-    .shift();
-}
 
 function UpdateLight(state)
 {
